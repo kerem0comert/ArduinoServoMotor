669be2d36d90d542f68dc2e8c97776a8db06d515
Khisant <ouzerturul@gmail.com>
2021-04-23 18:44:33 +0300

    Simplify arduino logic

--- raspberrySide/arduinoread.c
+++ raspberrySide/arduinoread.c
@@ -20,7 +20,7 @@ const int POTENT_PIN = 0;
 String incomingData;
 String positionString;
 char readChar;
-char carray1[6]; //magic needed to convert string to a number 
+char intValue[6]; //magic needed to convert string to a number 
 
 void setup() {
   servoTilt.attach(TILT_PIN);  
@@ -32,52 +32,37 @@ void setup() {
 }
 
 void loop() {
-
-
-  if(Serial.available()){         //From RPi to Arduino
-    
-    if(Serial.available() > 0){
-      readChar = Serial.read();  //gets one byte from serial buffer
-      incomingData += readChar; //makes the string incomingData
-      
-      counter ++;
-      //Serial.println(counter);
-    }
-    if(incomingData.length() > 0){
-      //Serial.println(incomingData);
-    }
-    if(counter == 5) dataType = readChar;
-       
-    if(counter == 6){
-      positionString = incomingData.substring(1,4);
-      //Serial.println(positionString);
-      incomingData = "";
-      positionString.toCharArray(carray1, sizeof(carray1));
-      if(dataType == 't'){
-        tiltValue = atoi(carray1); 
-        servoTilt.write(tiltValue); // sets the servo position according to the scaled value
+  if(Serial.available()){ 
+      readChar = Serial.read();    //From RPi to Arduino
+      if(readChar == '*') {
+        //Serial.println(incomingData);
+        operateOnData();
       }
-      else if(dataType == 'p'){
-        panValue = atoi(carray1);
-          servoPan.write(panValue);
-      }else if(dataType == 'w'){
-        powerValue = atoi(carray1);
-        analogWrite(BACKWARD_PIN, 0);
-        analogWrite(FORWARD_PIN,  powerValue);   //powerValue
-      }else if(dataType == 's'){
-        powerValue = atoi(carray1);
-        analogWrite(FORWARD_PIN, 0);
-        analogWrite(BACKWARD_PIN, powerValue);
-      }
-      
-      counter = 0;
-    }
+      else incomingData += readChar; //append each byte
+  }
 }
 
-  //printSerial();
-  Serial.println(analogRead(POTENT_PIN));
-  delay(SERVO_DELAY);                           // waits for the servo to get there
-
+void operateOnData(){
+  String incomingValue = incomingData.substring(1,4);
+  char incomingType = incomingData[4];
+  Serial.println("Position value: " + incomingValue + " Data type: " + incomingType);
+  incomingData = "";
+  incomingValue.toCharArray(intValue, sizeof(intValue));
+  if(incomingType == 't'){
+    tiltValue = atoi(intValue); 
+    servoTilt.write(tiltValue); // sets the servo position according to the scaled value
+  }else if(incomingType == 'p'){
+    panValue = atoi(intValue);
+    servoPan.write(panValue);
+  }else if(incomingType == 'w'){
+    powerValue = atoi(intValue);
+    analogWrite(BACKWARD_PIN, 0);
+    analogWrite(FORWARD_PIN,  powerValue);   //powerValue
+  }else if(incomingType == 's'){
+    powerValue = atoi(intValue);
+    analogWrite(FORWARD_PIN, 0);
+    analogWrite(BACKWARD_PIN, powerValue);
+  }
 }
 
 
